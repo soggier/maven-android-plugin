c3ab68562000cee80ce7e0fad643c2f2f8b26195
Manfred Moser <manfred@simpligility.com>
2014-05-06 20:57:13 -0700

    in progress commit to change branch, verifier test sort of getting to work

--- src/test/java/com/jayway/maven/plugins/android/standalonemojos/ZipalignMojoVerifierTest.java
+++ src/test/java/com/jayway/maven/plugins/android/standalonemojos/ZipalignMojoVerifierTest.java
@@ -1,5 +1,6 @@
 package com.jayway.maven.plugins.android.standalonemojos;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -7,6 +8,8 @@ import java.io.File;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.junit.Test;
+
+import com.jayway.maven.plugins.android.VerifierTestHelper;
 
 public class ZipalignMojoVerifierTest  {
 
@@ -19,4 +22,13 @@ public class ZipalignMojoVerifierTest  {
         Verifier verifier = new Verifier(testFolder.getAbsolutePath());
         assertTrue(verifier != null);
     }
+    
+    @Test
+    public void testZipalignMojoDefaultConfig() throws VerificationException
+    {
+        String testProjectFolder = VerifierTestHelper.getTestProjectFolder("zipalign-config-project0");
+        Verifier verifier = new Verifier(testProjectFolder);
+        verifier.executeGoal("com.jayway.maven.plugins.android.generation2:android-maven-plugin:zipalign");
+    }
+
 }
